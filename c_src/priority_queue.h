da690f61a9b7815c0e0d833bb050ead34627aa20
Silviu Caragea <silviu.caragea@woowteam.com>
2016-08-06 21:31:12 +0300

    Cleanup header

--- c_src/priority_queue.h
+++ c_src/priority_queue.h
@@ -9,26 +9,26 @@ typedef void(*UpdatePositionFun)(void*, int);
 class PriorityQueue
 {
 public:
-    
+
     PriorityQueue(LessFun ls, UpdatePositionFun upd);
     ~PriorityQueue();
-    
-    int size() const { return length_;}
-    bool insert (void* item);
+
+    bool insert(void* item);
     bool remove(void* item, int pos);
     void* remove(int pos);
     void* pop() {return remove(0);}
     void* peek();
-    
+    int size() const { return length_;}
+
 private:
-    
+
     DISALLOW_COPY_AND_ASSIGN(PriorityQueue);
-    
+
     inline void set(int pos, void* item);
     inline void swap(int pos1, int pos2);
     void bubble_down(int pos);
     void bubble_up(int pos);
-    
+
     int capacity_;
     int length_;
     void** heap_;
